1691a4a0d60ef56930da7dacbe68c2569958e288
Evgeni Serebriakov <e.serebriakov@topface.com>
2017-05-25 13:15:15 +0300

    Add uglify plugin

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const appID = '1dce933d8a538b1249eb917fe70171cf';
-export const apiUrl = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/';
+export const apiUrl = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const env = process.env.NODE_ENV;
 const isProd = process.env.NODE_ENV === 'prod';
@@ -10,9 +10,34 @@ const isDev = !isProd;
 function getDevtool() {
   let devtool = 'source-map';
   if (isProd) {
-    devtool = null;
+    devtool = false;
   }
   return devtool;
+}
+
+function getPlugins() {
+  const plugins = [
+    new CleanWebpackPlugin(['public'], {
+      root: __dirname,
+      verbose: true,
+      dry: false
+    }),
+    new webpack.DefinePlugin({
+      WP_NODE_ENV: JSON.stringify(env),
+      WP_IS_DEV: isDev,
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+  ];
+
+  if (isProd) {
+    plugins.push(
+      new UglifyJSPlugin()
+    );
+  }
+
+  return plugins;
 }
 
 module.exports = {
@@ -63,19 +88,5 @@ module.exports = {
 
   devtool: getDevtool(),
 
-  plugins: [
-    new CleanWebpackPlugin(['public'], {
-      root: __dirname,
-      verbose: true,
-      dry: false
-    }),
-    new webpack.DefinePlugin({
-      WP_NODE_ENV: JSON.stringify(env),
-      WP_IS_DEV: isDev,
-      'process.env': {
-        NODE_ENV: JSON.stringify(env)
-      }
-    }),
-    new ExtractTextPlugin('[name].css'),
-  ],
+  plugins: getPlugins(),
 };
